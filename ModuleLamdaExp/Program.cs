8de6fc4f99200423bf46df923a891a4ec938f585
Dan Edens <Dan.Edens@Geo-instruments.com>
2022-08-13 19:50:08 -0400

    writes simple square method

--- ModuleLamdaExp/Program.cs
+++ ModuleLamdaExp/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Square(5));
+        }
 
+        static int Square(int number)
+        {
+            return number*number;
         }
     }
 }
