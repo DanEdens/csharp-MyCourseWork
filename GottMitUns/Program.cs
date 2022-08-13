02247c8efd5fd0b6df77653111709e23c82bdd8d
Dan Edens <Dan.Edens@Geo-instruments.com>
2022-08-13 14:05:19 -0400

    adds default constructor and finishes module 6

--- GottMitUns/Program.cs
+++ GottMitUns/Program.cs
@@ -3,7 +3,7 @@
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
             var number = new Nullable<int>();
             Console.WriteLine("Has Value ?" + number.HasValue);
             Console.WriteLine("Value: " + number.GetValueorDefault());
