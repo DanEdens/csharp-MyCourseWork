9d1160905803fcadfb62f7ca38574431b0f2df2d
Dan Edens <Dan.Edens@Geo-instruments.com>
2022-08-13 13:06:57 -0400

    teachs about constraints

--- GottMitUns/Utilities.cs
+++ GottMitUns/Utilities.cs
@@ -1,13 +1,10 @@
 ﻿namespace Generics
 {
-    public class Utilities
+    public class Utilities<T> where T : IComparable 
     {
-        public int Max(int a, int b)
-        { 
-            return a > b ? a : b 
-        }
+        public int Max(int a, int b) => a > b ? a : b;
 
-        public T Max<T>(T a, T b) where T : IComparable
+        public T Max(T a, T b)
         {
             return a.CompareTo(b) > 0 ? a : b;
         }
