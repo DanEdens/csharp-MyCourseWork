3406e6d104129a6e1acd9715166c162db7828c10
Dan Edens <Dan.Edens@Geo-instruments.com>
2022-08-13 13:03:36 -0400

    learns to create a generic method inside a non generic class

---
+++ GottMitUns/Utilities.cs
@@ -0,0 +1,15 @@
+﻿namespace Generics
+{
+    public class Utilities
+    {
+        public int Max(int a, int b)
+        { 
+            return a > b ? a : b 
+        }
+
+        public T Max<T>(T a, T b) where T : IComparable
+        {
+            return a.CompareTo(b) > 0 ? a : b;
+        }
+    }
+} 
