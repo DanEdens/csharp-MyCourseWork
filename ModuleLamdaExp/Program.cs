ccb80ffefd27ba43d9fad2db837558b084095fe5
Dan Edens <Dan.Edens@Geo-instruments.com>
2022-08-13 19:48:31 -0400

    sets up main

--- ModuleLamdaExp/Program.cs
+++ ModuleLamdaExp/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace LamedaExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
