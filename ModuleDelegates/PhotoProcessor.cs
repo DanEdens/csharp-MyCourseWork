a0265d95333f3976b2379193eead003c7d913311
Dan Edens <Dan.Edens@Geo-instruments.com>
2022-08-13 18:00:36 -0400

    uses delegate instead of PhotoFilterHandler

--- ModuleDelegates/PhotoProcessor.cs
+++ ModuleDelegates/PhotoProcessor.cs
@@ -2,9 +2,7 @@
 {
     public class PhotoProcessor
     {
-        public delegate void PhotoFilterHandler(Photo photo);
-
-        public void Process(string path, PhotoFilterHandler filterHandler)
+        public void Process(string path, Action<Photo> filterHandler)
         {
             var photo = Photo.Load(path);
 
