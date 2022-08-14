7ac8dcf5ada51a9616fe4482193e459ab7677347
Dan Edens <Dan.Edens@Geo-instruments.com>
2022-08-14 16:52:33 -0400

    creates string extension method

---
+++ ModuleExtensionMethods/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace ModuleExtensionMethods
+{
+    public static class StringExtensions
+    {
+        public static string Shorten(this String str, int numberofwords)
+        {
+            if (numberofwords <= 0)
+                throw new ArgumentOutOfRangeException("numberOfWords should be greater than or equal to 0.");
+            if (numberofwords == 0)
+                return "";
+            var words = str.Split(' ');
+
+            if (words.Length <= numberofwords)
+                return str;
+
+            return string.Join(" ",words.Take(numberofwords));
+        
+        }
+    }
+}
