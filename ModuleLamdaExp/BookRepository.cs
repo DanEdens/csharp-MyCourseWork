a51acd9a9dfc80735634c9c921a2e0d1e23715b0
Dan Edens <Dan.Edens@Geo-instruments.com>
2022-08-13 20:05:49 -0400

    adds BookRepository class

---
+++ ModuleLamdaExp/BookRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace LamedaExpressions
+{
+    public class BookRepository
+    { 
+        public List<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book() {Title = "Title 1", Price = 5},
+                new Book() {Title = "Title 2", Price = 7},
+                new Book() {Title = "Title 3", Price = 17}
+            };
+        }
+    }
+}
