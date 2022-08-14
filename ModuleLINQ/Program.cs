3e95dd20ceb712fa734c0e8211415ad59bba4b34
Dan Edens <Dan.Edens@Geo-instruments.com>
2022-08-14 19:06:42 -0400

    Adds more methods to only print Title of books

--- ModuleLINQ/Program.cs
+++ ModuleLINQ/Program.cs
@@ -10,10 +10,10 @@ namespace ModuleLINQ
         {
             var books = new BookRepository().GetBooks();
 
-            var cheapBoooks = books.Where(b => b.Price < 10);
+            var cheapBoooks = books.Where(b => b.Price < 10).OrderBy(b => b.Title).Select(b => b.Title);
     
            foreach (var book in cheapBoooks)
-                Console.WriteLine(book.Title + " " + book.Price);
+                Console.WriteLine(book);
         }
     }
 }
