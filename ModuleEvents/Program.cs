72aa40ca6c35d58e487163c3188e4f7647c90d1f
Dan Edens <Dan.Edens@Geo-instruments.com>
2022-08-14 15:48:38 -0400

    adds subscriber

--- ModuleEvents/Program.cs
+++ ModuleEvents/Program.cs
@@ -5,7 +5,8 @@
         static void Main(string[] args)
         {
             var video = new Video() { Title = "Video 1" };
-            var videoEncoder = new VideoEncoder();
+            var videoEncoder = new VideoEncoder(); //publisher
+            var mailService = new MailService(); //subscriber
 
             videoEncoder.Encode(video);
         }
