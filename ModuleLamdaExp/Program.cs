92bced0665871fc973e2ec6c7df27ad2e5f1de51
Dan Edens <Dan.Edens@Geo-instruments.com>
2022-08-13 20:24:04 -0400

    converts to lamda
    finishes module 8

--- ModuleLamdaExp/Program.cs
+++ ModuleLamdaExp/Program.cs
@@ -8,7 +8,7 @@ namespace LamedaExpressions
         {
             var books = new BookRepository().GetBooks();
             
-            var cheapBooks = books.FindAll(IsCheaperThan10Dollars);
+            var cheapBooks = books.FindAll(b => b.Price < 10);
 
             foreach (var book in cheapBooks)
             {
@@ -16,10 +16,5 @@ namespace LamedaExpressions
             }
 
         }
-
-        static bool IsCheaperThan10Dollars(Book book)
-        {
-            return book.Price < 10;
-        }
-   }
+    }
 }
