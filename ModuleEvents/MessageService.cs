2312563a28d0a97632be60eac2e61ae295e24b9b
Dan Edens <Dan.Edens@Geo-instruments.com>
2022-08-14 16:15:27 -0400

    adds video title to args

--- ModuleEvents/MessageService.cs
+++ ModuleEvents/MessageService.cs
@@ -2,9 +2,9 @@
 {
     public class MessageService
     {
-        public void OnVideoEncoded(object source, EventArgs args)
+        public void OnVideoEncoded(object source, VideoEventArgs args)
         {
-            Console.WriteLine("MessageService: Sending a text message...");
+            Console.WriteLine("MessageService: Sending a text message..." + args.Video.Title);
         }
     }
 }
