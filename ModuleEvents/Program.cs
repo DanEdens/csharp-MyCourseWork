4e75064771fbb62770397e03d5c0c8c0e3c5c037
Dan Edens <Dan.Edens@Geo-instruments.com>
2022-08-14 16:07:20 -0400

    sends email and fixes typo

--- ModuleEvents/Program.cs
+++ ModuleEvents/Program.cs
@@ -7,8 +7,10 @@
             var video = new Video() { Title = "Video 1" };
             var videoEncoder = new VideoEncoder(); //publisher
             var mailService = new MailService(); //subscriber
+            var messageService = new MessageService(); //subscriber
 
             videoEncoder.VideoEncoded += mailService.onVideoEncoded;
+            videoEncoder.VideoEncoded += messageService.OnVideoEncoded;
 
             videoEncoder.Encode(video);
         }
