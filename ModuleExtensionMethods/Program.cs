f811669c891cc58a3068d4ace01a978312e04bff
Dan Edens <Dan.Edens@Geo-instruments.com>
2022-08-14 17:01:28 -0400

    adds extension to string

--- ModuleExtensionMethods/Program.cs
+++ ModuleExtensionMethods/Program.cs
@@ -1,11 +1,13 @@
-﻿namespace ModuleExtensionMethods
+﻿using System;
+
+namespace ModuleExtensionMethods
 {
     class Program
     {
         static void Main(string[] args)
         {
             string post = "This is supposed to be a very long blog post...";
-            var ShortendPost = post.Shorten(5);
+            var ShortendPost = post.Shorten(5)
 
             Console.WriteLine(ShortendPost);
         }
