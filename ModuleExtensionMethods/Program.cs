5f8f0595d5f7e3132c61d38bed35a6d707e0c6cc
Dan Edens <Dan.Edens@Geo-instruments.com>
2022-08-14 17:03:32 -0400

    uses an extension instead of creating one
    squash

--- ModuleExtensionMethods/Program.cs
+++ ModuleExtensionMethods/Program.cs
@@ -7,9 +7,13 @@ namespace ModuleExtensionMethods
         static void Main(string[] args)
         {
             string post = "This is supposed to be a very long blog post...";
-            var ShortendPost = post.Shorten(5)
+            var ShortendPost = post.Shorten(5);
+
+            IEnumerable<int> numbers = new List<int>() { 1,5,3,10,2,18};
+            var max = numbers.Max();
 
             Console.WriteLine(ShortendPost);
+            Console.WriteLine(max);
         }
     }
 }
