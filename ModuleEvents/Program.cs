ddc4b1ddf4539b4ddde83c8633b195e3936a5917
Dan Edens <Dan.Edens@Geo-instruments.com>
2022-08-14 15:27:29 -0400

    Sets up example

--- ModuleEvents/Program.cs
+++ ModuleEvents/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ModuleEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var video = new Video() { Title = "Video 1" };
+            var videoEncoder = new VideoEncoder();
+
+            videoEncoder.Encode(video);
+        }
+    }
+}
---
+++ ModuleEvents/Video.cs
@@ -0,0 +1,7 @@
+﻿namespace ModuleEvents
+{
+    public class Video
+        {
+            public string Title { get; set; }
+        } 
+}
