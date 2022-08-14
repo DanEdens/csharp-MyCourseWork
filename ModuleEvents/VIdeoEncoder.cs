4f9b6f0eb9f1c1fd6cc1af9ff4d88c8c035afd1e
Dan Edens <Dan.Edens@Geo-instruments.com>
2022-08-14 16:18:06 -0400

    Shows shorter way to write it with prebuild delegates

--- ModuleEvents/VIdeoEncoder.cs
+++ ModuleEvents/VIdeoEncoder.cs
@@ -14,8 +14,11 @@ namespace ModuleEvents
         // 3- Raise the event
 
         public delegate void VideoEncodedEventHandler(object source, VideoEventArgs args);
+
+        // EventHandler 
+        // EventHandler<TEventArgs>
         
-        public event VideoEncodedEventHandler VideoEncoded;
+        public event EventHandler<VideoEventArgs> VideoEncoded;
         public void Encode(Video video)
         {
             Console.WriteLine("Encoding Video...");
