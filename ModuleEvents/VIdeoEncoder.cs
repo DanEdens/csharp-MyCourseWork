b433c3fe818c6a2bd586a41f79eb49798dc59109
Dan Edens <Dan.Edens@Geo-instruments.com>
2022-08-14 15:27:29 -0400

    Sets up example

---
+++ ModuleEvents/VIdeoEncoder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+
+namespace ModuleEvents
+{ 
+    public class VideoEncoder
+    {
+        public void Encode(Video video)
+        {
+            Console.WriteLine("Encoding Video...");
+            Thread.Sleep(1000);
+        }
+    }
+}
