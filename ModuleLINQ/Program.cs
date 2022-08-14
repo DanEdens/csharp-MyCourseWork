762ad44e62e2ca6b5ab0c017f91df2ebe337ee31
Dan Edens <Dan.Edens@Geo-instruments.com>
2022-08-14 19:21:03 -0400

    Linq for a single book

--- ModuleLINQ/Program.cs
+++ ModuleLINQ/Program.cs
@@ -10,20 +10,9 @@ namespace ModuleLINQ
         {
             var books = new BookRepository().GetBooks();
 
-            // LinQ query Operators
-            var cheaperBooks = from b in books
-                               where b.Price < 10
-                               orderby b.Title
-                               select b.Title;
+            var book = books.Single(b => b.Title == "ASP.Net MVC");
 
-            // LINQ extension Methods
-            var cheapBoooks = books
-                .Where(b => b.Price < 10)
-                .OrderBy(b => b.Title)
-                .Select(b => b.Title);
-    
-           foreach (var book in cheapBoooks)
-                Console.WriteLine(book);
+            Console.WriteLine(book.Title);
         }
     }
 }
