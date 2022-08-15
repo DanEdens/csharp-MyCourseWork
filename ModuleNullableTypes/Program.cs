157bc0c1af6f37a8605f6907eeb09e371070ba6b
Dan Edens <Dan.Edens@Geo-instruments.com>
2022-08-14 22:18:10 -0400

    another way to do the same thing

--- ModuleNullableTypes/Program.cs
+++ ModuleNullableTypes/Program.cs
@@ -8,6 +8,7 @@ namespace ModuleNullableTypes
         {
             DateTime? date = new DateTime(2014, 1, 1);
             DateTime date2 = date.GetValueOrDefault();
+            DateTime? date3 = date2; 
 
             Console.WriteLine( date2);
         }
