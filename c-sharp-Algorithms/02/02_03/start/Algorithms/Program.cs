80624841f3007b4532f371f8cbaeb9e8e7213933
John Edens <dan.edens@geo-instruments.com>
2022-08-24 10:14:49 -0400

    init lesson 02_03

---
+++ c-sharp-Algorithms/02/02_03/start/Algorithms/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Algorithms {
+    class Program {
+        static int[] FindEvenNums(int[] arr1, int[] arr2) {
+            return null;
+        }
+
+        static void Main(string[] args) {
+            int[] arr1 = {-8, 2, 3, -9, 11, -20};
+            int[] arr2 = {0, -2, -9, -39, 39, 10, 7};
+
+            int[] evenArr = FindEvenNums(arr1, arr2);
+        }
+    }
+}
