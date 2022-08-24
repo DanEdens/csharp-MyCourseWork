1e72a6a572fc23f2c9c2426d8f640c73622e823c
John Edens <dan.edens@geo-instruments.com>
2022-08-24 10:32:54 -0400

    another method for reverses array

--- c-sharp-Algorithms/02/02_04/start/Algorithms/Program.cs
+++ c-sharp-Algorithms/02/02_04/start/Algorithms/Program.cs
@@ -11,9 +11,19 @@ namespace Algorithms {
             return reversed
         }
 
+        static void ReverseInPlace(int[] input) {
+            for (int i = 0; i < input.Length / 2; i++) {
+                int temp = input[i];
+                input[i] = input[input.Length - i -1];
+                input[input.Length - 1 -i] = temp;
+            }
+        }
         static void Main(string[] args) {
             int[] arr = { 1, 2, 3, 4, 5, 6 };
             Array.ForEach(Reverse(arr), Console.WriteLine);
+            ReverseInPlace(arr);
+            Array.ForEach(arr, Console.WriteLine);
+
         }
     }
 }
