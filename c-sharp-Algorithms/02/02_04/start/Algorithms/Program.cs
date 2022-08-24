444e3ae59aa93717847b421aa06c2255baa3976a
John Edens <dan.edens@geo-instruments.com>
2022-08-24 10:21:22 -0400

    init lesson 4

---
+++ c-sharp-Algorithms/02/02_04/start/Algorithms/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Algorithms {
+    class Program {
+        static int[] Reverse(int[] input) {
+            return input;
+        }
+
+        static void Main(string[] args) {
+            int[] arr = { 1, 2, 3, 4, 5, 6 };
+            Array.ForEach(Reverse(arr), Console.WriteLine);
+        }
+    }
+}
