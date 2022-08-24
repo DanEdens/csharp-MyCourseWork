03c5e9f58f9b26523ec21fe399e723c0718f8909
John Edens <dan.edens@geo-instruments.com>
2022-08-24 10:18:56 -0400

    pulls just even numbers from arrays

--- c-sharp-Algorithms/02/02_03/start/Algorithms/Program.cs
+++ c-sharp-Algorithms/02/02_03/start/Algorithms/Program.cs
@@ -3,7 +3,20 @@
 namespace Algorithms {
     class Program {
         static int[] FindEvenNums(int[] arr1, int[] arr2) {
-            return null;
+            ArrayList Result = new ArrayList();
+            
+
+            foreach(int num in arr1) {
+                if (num % 2 == 0) {
+                result.Add(num);
+                }
+        
+
+            foreach(int num in arr2) {
+                 if (num % 2 == 0) {
+                result.Add(num);
+                }
+
         }
 
         static void Main(string[] args) {
