29cf7ed7bd3a329d86e2a2fb456871ef7545d11d
John Edens <dan.edens@geo-instruments.com>
2022-08-24 10:00:41 -0400

    init lesson 2 algorithms

---
+++ c-sharp-Algorithms/02/02_02/start/Algorithms/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Algorithms {
+    class Program {
+
+        static Boolean BinarySearch(int[] inputArray, int item) {
+            return false;
+        }
+
+        static void Main(string[] args) {
+            int[] arr = {1, 2, 3, 4, 5, 6 };
+        }
+    }
+}
